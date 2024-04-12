4b761550c8c4cdd04a9feca22b0bdc2ead6264d8
Gaurav <program.drgk@gmail.com>
2024-04-11 21:43:58 -0700

    Initial commit & assests ADDED

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+// import Image from "next/image";
+import HomePage from "./homepage/page";
+import NavigationPage from "./menu/page";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      {/* <h1>The Game Vault</h1> */}
+      <Link href="/">
+        <HomePage />
+      </Link>
+      {/* <Link href="/menu">
+        <NavigationPage />
+      </Link> */}
+    </>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+
+// export default nextConfig;
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080",
+        pathname: "/static-files/images/**",
+      },
+    ],
+  },
+  //   async rewrites() {
+  //     return [
+  //       {
+  //         source: "/games/*",
+  //         destination: "http://localhost:8080/games/*",
+  //       },
+  //     ];
+  //   },
+};
 
 export default nextConfig;
+
+// module.exports = {
+
+// };
