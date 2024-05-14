41fd1cbb20c0ceeb3e3aabbedaec2275fd6fed41
Gaurav <program.drgk@gmail.com>
2024-05-13 22:29:35 -0700

    Mock data for Game Product details page Setup DONE

---
+++ __mocks__/singleGameMock.js
@@ -0,0 +1,23 @@
+[
+  {
+    id: 1,
+    title: "Hogwarts Legacy",
+    release_date: "February 10, 2023",
+    description:
+      "Hogwarts Legacy is an upcoming action role-playing video game set in the Harry Potter universe.",
+    price: "$59.99",
+    price_15days: "$20.99",
+    price_30days: "$35.99",
+    platform: "Xbox One  Xbox Series X | S",
+    genre: "Action Role-Playing Open world",
+    developer: "Portkey Games",
+    players: "Single player",
+    online_play: "Single player offline",
+    ratings: "4.5 / 5",
+    image1: `${process.env.DB_STATIC}/static-files/images/HogwartsLegacy/Hogwarts1.webp`,
+    image2: `${process.env.DB_STATIC}/static-files/images/HogwartsLegacy/Hogwarts2.jpeg`,
+    image3: `${process.env.DB_STATIC}/static-files/images/HogwartsLegacy/Hogwarts3.jpeg`,
+    image4: `${process.env.DB_STATIC}/static-files/images/HogwartsLegacy/Hogwarts4.jpeg`,
+    image5: `${process.env.DB_STATIC}/static-files/images/HogwartsLegacy/Hogwarts5.jpeg`,
+  },
+];
--- __tests__/xboxProductDetails.test.js
+++ __tests__/xboxProductDetails.test.js
@@ -1,46 +1,62 @@
-// import "@testing-library/jest-dom";
-// import { render, screen, waitFor } from "@testing-library/react";
-// import axios from "axios";
-// import XboxProductDetailsPage from "@/app/games/xbox/[id]/page";
-// import "../app/games/xbox/page";
+import "@testing-library/jest-dom";
+import { act, render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
+import XboxProductDetailsPage from "@/app/games/xbox/[id]/page";
+import singleGameMock from "../__mocks__/singleGameMock";
 
-// jest.mock("../app/games/xbox/page");
-// jest.mock("axios");
+jest.mock("axios");
 
-// describe("should render Xbox Product Details Page", () => {
-//   beforeEach(() => {
-//     axios.get.mockReset();
-//   });
+describe("should render Xbox Product Details Page", () => {
+  beforeEach(() => {
+    axios.get.mockReset();
+  });
 
-//   test("render Product details page with id", async () => {
-//     const mockSingleGame = {
-//       id: 1,
-//       title: "Hogwarts Legacy",
-//       release_date: "February 10, 2023",
-//       description:
-//         "Hogwarts Legacy is an upcoming action role-playing video game set in the Harry Potter universe.",
-//       price: "$59.99",
-//       price_15days: "$20.99",
-//       price_30days: "$35.99",
-//       platform: "Xbox One  Xbox Series X | S",
-//       genre: "Action Role-Playing Open world",
-//       developer: "Portkey Games",
-//       players: "Single player",
-//       online_play: "Single player offline",
-//       ratings: "4.5 / 5",
-//       image1: `${process.env.DB_STATIC}/static-files/images/HogwartsLegacy/Hogwarts1.webp`,
-//       image2: `${process.env.DB_STATIC}/static-files/images/HogwartsLegacy/Hogwarts2.jpeg`,
-//       image3: `${process.env.DB_STATIC}/static-files/images/HogwartsLegacy/Hogwarts3.jpeg`,
-//       image4: `${process.env.DB_STATIC}/static-files/images/HogwartsLegacy/Hogwarts4.jpeg`,
-//       image5: `${process.env.DB_STATIC}/static-files/images/HogwartsLegacy/Hogwarts5.jpeg`,
-//     };
-//     axios.get.mockResolvedValueOnce({ singleGame: mockSingleGame });
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
 
-//     render(<XboxProductDetailsPage match={{ params: { id: 1 } }} />);
+  test("render Product details page WITHOUT ID", async () => {
+    render(<XboxProductDetailsPage params={{ params: { id: undefined } }} />);
+    const getSingleGame = jest.fn();
+    const singleGameData = await getSingleGame();
+    // act()
+    expect(setSingleGames).toBe("");
+    expect(singleGameData).toBe("");
+    expect(screen.getByText("[G]")).toBeInTheDocument();
+  });
 
-//     await waitFor(() => {
-//       expect(axios.get).toHaveBeenCalledWith(`/games/xbox/${params.params.id}`);
-//       expect(screen.getByText("Hogwarts Legacy"));
-//     });
-//   });
-// });
+  //   test("render Product details page with id", async () => {
+  //     axios.get.mockResolvedValueOnce({ singleGame: singleGameMock });
+
+  //     render(
+  //       <XboxProductDetailsPage
+  //         // params={({ params: { id: 1 } }, { singleGame: { singleGameMock } })}
+  //         params={{ params: { id: 1 } }}
+  //       />
+  //     );
+
+  //     // await waitFor(() => {
+  //     //   //   expect(axios.get).toHaveBeenCalledWith(`/games/xbox/1`);
+  //     //   expect(axios.get).toHaveBeenCalledWith(
+  //     //     `${process.env.NEXT_PUBLIC_BASE_URL}/games/xbox/1`
+  //     //   );
+  //     //   expect(screen.getByText("Hogwarts Legacy")).toBeInTheDocument();
+  //     // });
+  //     const getSingleGame = jest.fn();
+  //     const singleGameData = await getSingleGame();
+  //     act(() => {});
+  //     expect(singleGameData).toBe(singleGameMock[0]);
+  //     expect(screen.getByText(/add to cart/i)).toBeInTheDocument();
+  //     // expect(screen.getByText(/hogwarts/i)).toBeInTheDocument();
+
+  //     // BASED ON DOCS
+  //     // axios.get.mockResolvedValue({
+  //     //   data: { singleGameMock },
+  //     // });
+  //     // const getSingleGame = jest.fn();
+  //     // const singleGameData = await getSingleGame();
+  //     // expect(singleGameData).toBe(singleGameMock[0]);
+  //     // // expect(getSingleGame).toHaveBeenCalledWith(1);
+  //     // render(<XboxProductDetailsPage match={{ params: { id: 1 } }} />);
+  //   });
+});
